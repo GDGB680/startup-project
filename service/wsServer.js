77b8143a0b77c53075885ee917ea9f23d3529116
George Brunt <dexter@brunts.org>
2025-12-02 15:40:42 -0700

    Update wsServer.js

--- service/wsServer.js
+++ service/wsServer.js
@@ -1,1 +1,10 @@
 import { WebSocketServer } from 'ws';
+
+class BountyHunterWS {
+  constructor(httpServer) {
+    this.wss = new WebSocketServer({ server: httpServer, path: '/ws' });
+    this.clients = new Map(); // Map of connected users
+    
+    this.wss.on('connection', (ws) => this.handleConnection(ws));
+  }
+}

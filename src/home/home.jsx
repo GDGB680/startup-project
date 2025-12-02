0d51d65f0d1ff0150585d4f7d77e845fe83c699c
George Brunt <dexter@brunts.org>
2025-12-02 15:54:37 -0700

    fixed the dependency

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
+import { LiveFeed } from '../components/LiveFeed';
 import { adviceWidget } from '../components/adviceWidget';
 
 export function Home() {
